f797a413d803077576a02f8eee62b1d12cc93b41
Jonatan Pålsson <jonatan.p@gmail.com>
2019-03-16 11:33:20 +0100

    util: Add find_git_dir
    This recursively looks through parent directories until it finds a .git
    directory

--- lib/util.c
+++ lib/util.c
@@ -2,17 +2,20 @@
 
 #include <dirent.h>
 #include <error.h>
+#include <limits.h>
 #include <stddef.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <sys/stat.h>
 
-int is_dir(char *dir) {
+int is_dir(const char *dir) {
     struct stat sb;
 
     return stat(dir, &sb) == 0 && S_ISDIR(sb.st_mode);
 }
 
-int num_entries_in_dir(char *dir) {
+int num_entries_in_dir(const char *dir) {
     int entry_count = -2; // Don't count . and ..
     DIR * dirp;
     struct dirent * entry;
@@ -32,4 +35,27 @@ int num_entries_in_dir(char *dir) {
     closedir(dirp);
 
     return entry_count;
+}
+
+char *find_git_dir(const char *dir) {
+    char check_path[PATH_MAX];
+    char parent_path[PATH_MAX];
+    snprintf(check_path, PATH_MAX, "%s/.git", dir);
+
+    if (is_dir(check_path)) {
+        return strndup(check_path, PATH_MAX);
+    }
+
+    snprintf(check_path, PATH_MAX, "%s/..", dir);
+    if (!realpath(check_path, parent_path)) {
+        ERROR ("Failed to recurse to parent directory");
+        return NULL;
+    }
+
+    if (!strncmp(dir, parent_path, PATH_MAX)) {
+        /* There is no .git directory before reaching / */
+        return NULL;
+    }
+
+    return find_git_dir(parent_path);
 }
--- lib/util.h
+++ lib/util.h
@@ -1,2 +1,3 @@
-int num_entries_in_dir (char *dir);
-int is_dir(char *dir);
+int num_entries_in_dir (const char *dir);
+int is_dir(const char *dir);
+char *find_git_dir(const char *dir);
